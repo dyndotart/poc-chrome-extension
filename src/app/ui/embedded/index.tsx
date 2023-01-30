e4da60e7fea10eb8799378aa28b31ddaec2dcfa4
benno <bennokohrs@gmail.com>
2023-01-30 08:44:32 +0100

    wip

--- src/app/ui/embedded/index.tsx
+++ src/app/ui/embedded/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import Routes from './routes';
 import './core';
+import $ from 'jquery';
 
 // Styles
 import '../../styles/tailwind.css';
@@ -20,7 +21,53 @@ function initReact() {
   );
 }
 
+function initReactSearch() {
+  let observer: MutationObserver | null = null;
+
+  // Inject HTML into Search Suggestions
+  $('#global-enhancements-search-query').on('input', async function () {
+    const searchTerm = $(this).val();
+
+    if (searchTerm != null) {
+      const suggestionNode = document.querySelector(
+        '#global-enhancements-search-suggestions'
+      );
+      if (suggestionNode != null) {
+        // Disconnect old Suggestions Element Observer
+        if (observer != null) {
+          observer.disconnect();
+          observer = null;
+        }
+
+        // Create new Observer to listen on Suggestion changes
+        // so the HTML can be injected on the new Suggestion Elements
+        observer = new MutationObserver(function () {
+          console.log('Debug: Init Search Elements for Term: ', searchTerm);
+          const suggestionContainer = $(
+            '#global-enhancements-search-suggestions'
+          );
+          let divElements = suggestionContainer.find('.as-entry');
+          divElements.each(function () {
+            $(this).append('<p>Test</p>');
+          });
+
+          // Disconnect Observer as soon as Elements of Suggestion Container have changed once
+          // -> New Suggestions were loaded
+          if (observer != null) {
+            observer.disconnect();
+            observer = null;
+          }
+        });
+
+        // Register Suggestion Element Observer
+        observer.observe(suggestionNode, { childList: true });
+      }
+    }
+  });
+}
+
 function init() {
+  initReactSearch();
   initReact();
 }
 
